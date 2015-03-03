310a45fb5c32313e8137c9b5a2011252d055b034
Shlomo Twerski <ystwerski@gmail.com>
2015-03-02 21:14:53 -0600

    Started app, added users with devise, and added bootstrap

---
+++ db/migrate/20150301222522_devise_create_users.rb
@@ -0,0 +1,55 @@
+class DeviseCreateUsers < ActiveRecord::Migration
+  def change
+    create_table(:users) do |t|
+      ## Database authenticatable
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name,          null: false, default: ""
+      t.datetime :date_of_birth
+      t.string :default_address,    null: false, default: ""
+      t.string :default_city,       null: false, default: ""
+      t.string :default_state,      null: false, default: ""
+      t.string :email,              null: false, default: ""
+      t.integer :total_attempts,    null: false, default: 0
+      t.integer :successful_attempts, null: false, default: 0
+      t.integer :failed_attempts,   null: false, default: 0
+      t.integer :total_sent,        null: false, default: 0
+      t.integer :reliability,       null: false, default: 0
+      t.decimal :money_collected, precision: 8, scale: 2,   null: false, default: 0
+      t.decimal :money_paid, precision: 8, scale: 2,        null: false, default: 0
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+      ## Confirmable
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
+
+      ## Lockable
+      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.datetime :locked_at
+
+
+      t.timestamps
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
+  end
+end
