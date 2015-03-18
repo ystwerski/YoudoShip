6175ce96885a42e5c00c944d75df24706266cf83
Shlomo Twerski <ystwerski@gmail.com>
2015-03-18 02:30:36 -0500

    Trying to get index to display relevant orders

--- app/controllers/takers_controller.rb
+++ app/controllers/takers_controller.rb
@@ -1,8 +1,19 @@
 class TakersController < ApplicationController
+
 	
 	def index
-		address = params[:travelling_from_address] + " " + params[:travelling_from_city + ", " + params[:travelling_from_state]
-		@list = Order.where(:shipping_from_address => address)
+		@address_from = params[:travelling_from_address] + " " + params[:travelling_from_city] + ", " + params[:travelling_from_state]
+		@address_from = Geocoder.coordinates(address_from)
+		list = ShippingFrom.near(@address_from, 10)
+		orders = []
+		list.each do |list_object|
+			orders << list_object.orders
+		end
+		@orders =[]
+		orders.each do |order|
+			@orders << order[0]
+		end
+
 	end
 
 	def new
